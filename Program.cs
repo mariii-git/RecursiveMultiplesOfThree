6508caab8b7e2ec9fb48f9532bbe101835b61858
mariiiiiii <masum.s1343@gmail.com>
2025-07-30 09:08:09 -0700

    multiplesof3

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOf3
+{
+    internal class Program
+    {
+        static void PrintMultiplesOf3(int n)
+        {
+            if (n <= 0)
+                return;
+            PrintMultiplesOf3(n - 1);
+            if (n % 3 == 0)
+                Console.WriteLine(n);
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("enter : ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"مضرب های سه  کوچکتر از {n} : ");
+            PrintMultiplesOf3(n - 1);
+            
+        }
+    }
+}
